bfa746042d2cf1b520cf64890a069ee5a9a90a73
Shekhar-rv <shekhar.rv@gmail.com>
2024-02-28 21:19:41 +0000

    Testing the preprocessing

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,4 +2,14 @@ import pandas as pd
 
 data = pd.read_csv("/de_project/src/data/car_prices.csv")
 
-print(data.info())
+# print(data.info())
+
+print(data.columns)
+
+print(f"Unique makes before: {data['make'].nunique()}")
+
+data['make'] = data['make'].str.title()
+
+print(f"Unique makes after: {data['make'].nunique()}")
+
+print(data['make'].unique())
